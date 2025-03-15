07b5ec82495ff5c3d99601ef4f3327cf02818147
Gorbi Ello Pasaribu <123230083@student.upnyk.ac.id>
2025-03-15 12:44:03 +0700

    LoginPage.java

--- QuizOOP_123230083_GorbiElloPasaribu/src/quizoop_123230083_gorbiellopasaribu/LoginPage.java
+++ QuizOOP_123230083_GorbiElloPasaribu/src/quizoop_123230083_gorbiellopasaribu/LoginPage.java
@@ -1,47 +1,47 @@
 package quizoop_123230083_gorbiellopasaribu;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.*;
 
-public class LoginPage extends JFrame{
+public class LoginPage extends JFrame {
     private JTextField userField;
     private JPasswordField passField;
     private JButton loginButton;
-    
-    public LoginPage(){
-        setTitle("Login Page");
-        setSize(720,480);
+
+    public LoginPage() {
+        setTitle("Login");
+        setSize(400, 250);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        setLayout(null);
-        
-        add (new JLabel("Username: "));
-        userField = new JTextField();
-        add (userField);
-        
-        add (new JLabel("Password: "));
-        passField = new JPasswordField();
-        add (passField);
-        
+        setLayout(new GridLayout(3, 2, 10, 10));
+
+        add(new JLabel("Username:"));
+        userField = new JTextField(20);
+        add(userField);
+
+        add(new JLabel("Password:"));
+        passField = new JPasswordField(20);
+        add(passField);
+
         loginButton = new JButton("Login");
-        add (loginButton);
-        
+        add(loginButton);
+
         loginButton.addActionListener(e -> {
             String username = userField.getText();
-            String password = new String (passField.getPassword());
-            if (username.equals("user") && password.equals("user")){
-               new MainMenuPage();
-               dispose();
-            }else {
-                JOptionPane.showMessageDialog(this, "Invalid", "Error", JOptionPane.ERROR_MESSAGE);
-            }  
+            String password = new String(passField.getPassword());
+            if (username.equals("user") && password.equals("user")) {
+                new MainMenuPage();
+                dispose();
+            } else {
+                JOptionPane.showMessageDialog(this, "Invalid Credentials!", "Error", JOptionPane.ERROR_MESSAGE);
+            }
         });
-        
+
         setVisible(true);
     }
+
     public static void main(String[] args) {
         new LoginPage();
     }
 }
-
