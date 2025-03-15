f694d5f189006343b30d96ebc49d29c3763494b1
Gorbi Ello Pasaribu <123230083@student.upnyk.ac.id>
2025-03-15 12:41:43 +0700

    MainMenuPage.java

--- QuizOOP_123230083_GorbiElloPasaribu/src/quizoop_123230083_gorbiellopasaribu/MainMenuPage.java
+++ QuizOOP_123230083_GorbiElloPasaribu/src/quizoop_123230083_gorbiellopasaribu/MainMenuPage.java
@@ -1,16 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package quizoop_123230083_gorbiellopasaribu;
+package moviebooking;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.*;
-/**
- *
- * @author Lab Informatika
- */
-public class MainMenuPage extends {
-    
+
+public class MainMenuPage extends JFrame {
+    public MainMenuPage() {
+        setTitle("Main Menu");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new GridLayout(4, 1));
+
+        String[] movies = {"The Lost Kingdom", "Echoes of Time", "Skyline Chase", "Frozen Shadows"};
+
+        for (String movie : movies) {
+            JButton movieButton = new JButton(movie);
+            add(movieButton);
+            movieButton.addActionListener(e -> {
+                new BookingPage(movie);
+                dispose();
+            });
+        }
+
+        setVisible(true);
+    }
 }
